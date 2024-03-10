44dbb3ee0dff65265e867fcb2dd9fd2e2e3abf2a
hex-aragon <0xaragon90@gmail.com>
2024-03-10 16:39:46 +0900

    first commit

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+APPNAME="token_123456"
+
+ENV_FILE=".env"
+PRIVATE_KEY=$(grep "PRIVATE_KEY" $ENV_FILE | cut -d '=' -f2)
+
+QUERY_URL="https://api.explorer.aleo.org/v1"
+BROADCAST_URL="${QUERY_URL}/testnet3/transaction/broadcast"
+
+snarkos developer deploy "${APPNAME}.aleo" \
+  --path "./build/" \
+  --private-key "${PRIVATE_KEY}" \
+  --query "${QUERY_URL}" \
+  --priority-fee 3000000 \
+  --broadcast "${BROADCAST_URL}" \
